05cf0e24f2e39d44a2dfad2b06979e2e066a2780
Ben Tettmar <bentettmar@yahoo.com>
2022-03-29 16:41:47 +0100

    Video support

--- routes/general.py
+++ routes/general.py
@@ -12,11 +12,13 @@ def index():
 
     if len(args) == 0:
         return flask.render_template("index.html")
+
     title = args.get("title", "")
     description = args.get("description", "")
     url = args.get("url", "")
     colour = args.get("colour", "") or args.get("color", "") or "000000"
     image = args.get("image", "")
+    video = args.get("video", "")
     author_name = args.get("author_name", "")
     author_url = args.get("author_url", "")
     provider_name = args.get("provider_name", "")
@@ -43,5 +45,6 @@ def index():
         embed_url=url,
         embed_colour=colour,
         embed_image=image,
+        embed_video=video,
         oembed_link=oembed_link
     )
