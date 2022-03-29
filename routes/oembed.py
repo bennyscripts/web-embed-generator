faa5dc8fab8224094e55a47fed6dbe0ff1d542df
Ben Tettmar <bentettmar@yahoo.com>
2022-03-29 16:25:18 +0100

    Add generator route to generate an oembed json

--- routes/oembed.py
+++ routes/oembed.py
@@ -1,3 +1,25 @@
 import flask
 
 blueprint = flask.Blueprint('oembed', __name__, url_prefix='/oembed')
+
+@blueprint.route("/")
+def generator():
+    args = flask.request.args
+
+    title = args.get("title", "")
+    author_name = args.get("author_name", "")
+    author_url = args.get("author_url", "")
+    provider_name = args.get("provider_name", "")
+    provider_url = args.get("provider_url", "")
+
+    oembed_json = {
+        "version": "1.0",
+        "type": "rich",
+        "title": title,
+        "author_name": author_name,
+        "author_url": author_url,
+        "provider_name": provider_name,
+        "provider_url": provider_url
+    }
+
+    return flask.jsonify(oembed_json)
